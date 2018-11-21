4ee0972325d2cc052f944f13349977e968b5e12e
Benjamin Tannenwald <benjamin.tannenwald@cern.ch>
2018-11-20 21:30:13 -0500

    added sample handler

---
+++ src/sampleHandler.C
@@ -0,0 +1,13 @@
+#include "../include/sampleHandler.h"
+
+sampleHandler::sampleHandler()
+{
+
+}
+
+sampleHandler::~sampleHandler() {}
+
+void sampleHandler::test()
+{
+  std::cout << "Test!" << endl;
+}
